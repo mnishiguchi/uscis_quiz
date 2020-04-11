20ce995985971bcb6c9a1254c991b7e8293a4838
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-04-11 11:29:35 -0400

    Add rendering.dart for debugging

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  // debugPaintSizeEnabled = true;
+  // debugPaintBaselinesEnabled = true;
+  // debugPaintPointersEnabled = true;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // debugShowMaterialGrid: true,
       title: 'USCIS Quiz',
       theme: ThemeData(
         primarySwatch: Colors.green,
