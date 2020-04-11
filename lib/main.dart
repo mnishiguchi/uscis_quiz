9f6e9f37031680b6459f74d3e784a7c992b9fd28
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-04-10 22:21:36 -0400

    Initialize with a counter app

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'USCIS Quiz',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: CounterPage(title: 'USCIS Quiz Home Page'),
+    );
+  }
+}
+
+class CounterPage extends StatefulWidget {
+  CounterPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _CounterPageState createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
