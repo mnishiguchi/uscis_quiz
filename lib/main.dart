8755607575760494b5d750d568ad2b57afeb7fbe
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-04-11 11:45:01 -0400

    Move counter page to a separate file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter/rendering.dart';
 
+import './pages/counter_page.dart';
+
 void main() {
   // debugPaintSizeEnabled = true;
   // debugPaintBaselinesEnabled = true;
@@ -17,54 +19,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: CounterPage(title: 'USCIS Quiz Home Page'),
-    );
-  }
-}
-
-class CounterPage extends StatefulWidget {
-  CounterPage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _CounterPageState createState() => _CounterPageState();
-}
-
-class _CounterPageState extends State<CounterPage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      home: CounterPage(title: 'USCIS Quiz Counter Page'),
     );
   }
 }
