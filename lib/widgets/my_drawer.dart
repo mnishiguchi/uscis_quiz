c1762041695eae433ff8183aa7bf2dcabb155a1c
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2020-04-18 09:32:07 -0400

    Add bookmark count chip to the drawer

--- lib/widgets/my_drawer.dart
+++ lib/widgets/my_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'package:uscisquiz/blocs/blocs.dart';
 import 'package:uscisquiz/pages/pages.dart';
 
 class MyDrawer extends StatelessWidget {
@@ -7,29 +9,45 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: Column(
-        children: <Widget>[
-          AppBar(
-            automaticallyImplyLeading: false,
-            title: Text('USCIS Quiz'),
+    return BlocBuilder<UscisQuizBloc, UscisQuizState>(
+      builder: (_, state) {
+        final bookmarkCount =
+            (state as UscisQuizStateLoaded).bookmarkedIds.length;
+
+        return Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(
+                automaticallyImplyLeading: false,
+                title: Text('USCIS Quiz'),
+              ),
+              ListTile(
+                leading: Icon(Icons.list),
+                title: Text('All Questions'),
+                onTap: () {
+                  Navigator.pushReplacementNamed(
+                      context, QuestionsPage.routeName);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.bookmark),
+                title: Text('Bookmarked Questions'),
+                trailing: Chip(
+                  label: Text(bookmarkCount.toString()),
+                  labelStyle: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 16.0,
+                  ),
+                ),
+                onTap: () {
+                  Navigator.pushReplacementNamed(
+                      context, BookmarkedQuestionsPage.routeName);
+                },
+              ),
+            ],
           ),
-          ListTile(
-            leading: Icon(Icons.list),
-            title: Text('All Questions'),
-            onTap: () {
-              Navigator.pushReplacementNamed(context, QuestionsPage.routeName);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.bookmark),
-            title: Text('Bookmarked Questions'),
-            onTap: () {
-              Navigator.pushReplacementNamed(context, BookmarkedQuestionsPage.routeName);
-            },
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
